1def2ee4a0f3553cbcd9fea2f64a41bb95599a38
Thakur Rudraksha Singh <rudrakshasingh587@gmail.com>
2025-11-28 13:56:23 +0530

    feat: added start page with background image, set custom app icon, and added TS declarations for JPG support

---
+++ src/app/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Image, Text, View, Pressable, StatusBar } from "react-native";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import bgImage from "../../assets/bg-image.jpg";
+
+export default function Start() {
+  const router = useRouter();
+
+  return (
+    <View className="flex-1">
+      <StatusBar translucent backgroundColor="transparent" />
+      <Image
+        source={bgImage}
+        className="w-full h-full"
+        style={{ resizeMode: "cover" }}
+      />
+
+      <SafeAreaView
+        edges={["bottom"]}
+        className="absolute inset-0 flex-1 justify-end"
+      >
+        <View className="mb-10 mx-10 items-center">
+          <Text className="text-3xl text-center font-bold text-white mb-4">
+            Best <Text className="text-red-500">Workouts</Text> For You
+          </Text>
+
+          <Pressable
+            className="bg-red-500 rounded-full py-4 w-full shadow-lg border border-white"
+            onPress={() => {
+              router.push("/(app)/(tabs)");
+            }}
+          >
+            <Text className="text-white text-center text-lg font-semibold">
+              Start Now
+            </Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
